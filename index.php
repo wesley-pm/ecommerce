5cab40a444aa6b85b47e63ac0141e034a1b90203
wesley-pm <wesley_ccb@hotmail.com>
2020-09-24 01:17:18 -0400

    criamos PageAdmin

--- index.php
+++ index.php
@@ -2,13 +2,26 @@
 
 require_once("vendor/autoload.php");
 
+use \Slim\Slim;
+use \Hcode\Page;
+use \Hcode\PageAdmin;
+
 $app = new \Slim\Slim();
+
 
 $app->config('debug', true);
 
 $app->get('/', function() {
     
 	$page = new Hcode\Page();
+
+	$page->setTpl("index");
+
+});
+
+$app->get('/admin', function() {
+    
+	$page = new Hcode\PageAdmin();
 
 	$page->setTpl("index");
 
